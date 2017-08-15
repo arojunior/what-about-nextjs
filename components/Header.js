6e36d685217624910e9014ee1ec0da72ce9b5380
Junior Oliveira <arojunior@gmail.com>
2017-08-15 18:32:03 -0300

    code highlight theme

--- components/Header.js
+++ components/Header.js
@@ -4,6 +4,10 @@ const Header = () =>
   <div>
     <Head>
       <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <link
+        rel="stylesheet"
+        href="//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/styles/atom-one-dark.min.css"
+      />
       <title>What about Next.js?</title>
     </Head>
   </div>
