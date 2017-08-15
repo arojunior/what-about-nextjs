5189d11cb7d3966bace2b022eca974f2b7561765
Junior Oliveira <arojunior@gmail.com>
2017-08-15 19:00:02 -0300

    setup page

---
+++ pages/presentation/2-hello-world.js
@@ -0,0 +1,14 @@
+import Layout from '../../layouts/Default'
+import Highlight from 'react-highlight'
+
+const HelloWorld = () =>
+  <Layout title="Hello World?">
+    <Highlight className="javascript">
+      {`
+/* pages/index.js */
+export default () => <div>Hello World</div>
+        `}
+    </Highlight>
+  </Layout>
+
+export default HelloWorld
